c13efaa0ee36e6acbb512b322302ea638b5c5511
ayodiya <devayodiya@gmail.com>
2024-03-22 21:24:34 +0100

    add input field component

---
+++ components/InputField.jsx
@@ -0,0 +1,51 @@
+import TextField from '@mui/material/TextField'
+import Box from '@mui/material/Box'
+
+export default function InputField ({
+  name,
+  error,
+  type,
+  onBlur,
+  onChange,
+  email,
+  label,
+  accept,
+  placeholder,
+  ...props
+}) {
+  return (
+    <Box>
+      <TextField
+        sx={{
+          '& .MuiInputBase-input': {
+            backgroundColor: 'white',
+            borderRadius: '5px'
+          }
+        }}
+        InputLabelProps={{
+          style: { color: 'black' }
+        }}
+        accept={accept}
+        type={type}
+        name={name}
+        error={error !== undefined}
+        fullWidth
+        onBlur={onBlur}
+        onChange={onChange}
+        placeholder={placeholder}
+        label={label}
+        variant='outlined'
+        {...props}
+      />
+      <Box
+        sx={{
+          color: 'red',
+          paddingTop: '6px',
+          fontSize: '11px'
+        }}
+      >
+        {error}
+      </Box>
+    </Box>
+  )
+}
